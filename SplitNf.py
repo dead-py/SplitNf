f149164e48b4f00f056e5c54083e02096468ce81
Jayme Klein <72773669+dead-py@users.noreply.github.com>
2021-02-18 18:29:31 -0300

    Add files via upload

--- SplitNf.py
+++ SplitNf.py
@@ -1,5 +1,5 @@
-# MODELO 33 2102 14564469000672 55 001 000091965 1 00233526 2
-#        UF A/M  CNPJ           MD SER Nº NOTA   T COD NUM   V
+# MODELO 33 2102 14.564.469/0006-72 55 001 000091965 1 00233526  2
+#        UF A/M  CNPJ               MD SER Nº NOTA   T COD NUM   V
 
 # UNIDADE FEDERAL 
 # ANO/MÊS
@@ -12,10 +12,28 @@
 # DÍGITO VERIFICADOR
 
 codigos_uf = [11, 12, 13, 14, 15, 16, 17, 21, 22, 23, 24, 25, 26, 27, 28, 29, 31, 32, 33, 35, 41, 42, 43, 50, 51, 52, 53]
+nomes_uf = ['RO','AC','AM','RR','PA','AP','TO','MA','PI','CE','RN','PB','PE','AL','SE','BA','MG','ES','RJ','SP','PR','SC','RS','MS','MT','GO','DF']
+series = {
+    1:'NF-e',
+    2:'CT-e',
+    3:'NFS-e',
+    4:'NFC-e',
+    5:'CF-e',
+    6:'MF-e',
+    7:'MDF-e',
+    8:'NFAe',
+    9:'NF Complementar',
+    10:'NF Denegada',
+    11:'NF Rejeitada',
+    12:'NF de Exportação',
+    13:'NF de Remessa'
+    }
 
 def split_nf(cod_nf='33210214564469000672550010000919651002335262'):
 
     modelo_nf = ''
+    unid_federal = ''
+
 
     while len(cod_nf) < 44:
         return 1
@@ -30,7 +48,7 @@ def split_nf(cod_nf='33210214564469000672550010000919651002335262'):
     codnum_nf = cod_nf[35:43]
     verif_nf = cod_nf[43]
 
-    if int(mod_nf) == 55:
+    if int(mod_nf) == 55:  # Checa o modelo do documento
         modelo_nf = "NF-e"
     
     elif int(mod_nf) == 57:
@@ -43,9 +61,20 @@ def split_nf(cod_nf='33210214564469000672550010000919651002335262'):
         modelo_nf = "CÓDIGO DE DOCUMENTO INVÁLIDO"
 
 
-    if int(uf_nf) not in codigos_uf:
+    if int(uf_nf) not in codigos_uf:  # Checa a unidade federal
         codigo_uf = "CÓDIGO DE UF INVÁLIDO"
 
-    pass
+    else:
+        uf_index = codigos_uf.index(int(uf_nf))
 
-split_nf()
+        unid_federal = nomes_uf[uf_index]
+
+
+    data = date_nf[2:4] + '/' + date_nf[0:2]
+    
+    cnpj = cnpj_nf[0:2] + '.' + cnpj_nf[2:5] + '.' + cnpj_nf[5:8] + '/' + cnpj_nf[8:12] + '-' + cnpj_nf[12:]
+
+
+    return uf_nf, unid_federal,  data, cnpj, modelo_nf, serie_nf, nota_nf, tipo_nf, codnum_nf, verif_nf
+
+print(split_nf())
